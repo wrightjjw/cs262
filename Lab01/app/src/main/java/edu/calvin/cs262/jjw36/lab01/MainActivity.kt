073de3743ca41e994e09412918ef2682abc2480f
Josh Wright <wright.jjw@gmail.com>
2019-09-20 17:00:56 -0400

    Lab03 init

---
+++ Lab01/app/src/main/java/edu/calvin/cs262/jjw36/lab01/MainActivity.kt
@@ -0,0 +1,14 @@
+package edu.calvin.cs262.jjw36.lab01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "Hello, world!")
+    }
+}
