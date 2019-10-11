80deaa1135793b8f78c5c0869e448b07785f51c3
Josh Wright <wright.jjw@gmail.com>
2019-10-11 19:33:45 -0400

    merge

--- Lab05/src/app/edit-todo/edit-todo.module.ts
+++ Lab05/src/app/edit-todo/edit-todo.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
+import { IonicModule } from '@ionic/angular';
 
+import { EditTodoPage } from './edit-todo.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: EditTodoPage
+  }
+];
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [EditTodoPage]
 })
-export class EditTodoModule { }
+export class EditTodoPageModule {}
